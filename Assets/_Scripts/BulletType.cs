6ed87babbb4da3e7cdf53c2a14d81d45b5b32c6b
Brian <46753834+Shankk@users.noreply.github.com>
2020-10-16 23:14:58 -0400

    Initial commit

---
+++ Assets/_Scripts/BulletType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum BulletType
+{
+    BULLET,
+    FAT,
+    PULSING,
+    RANDOM
+}
